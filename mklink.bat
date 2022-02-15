6df9f9e513007e5d4592e5d6aea737a6efbc45c8
edayot <pro.e.dayot@gmail.com>
2022-02-15 20:14:14 +0100

    mklink change

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,4 @@
 
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\saves\New World (23)\datapacks\EnergyUtils DataPack" "C:\Users\erwan\Documents\Dev\EnergyUtils\EnergyUtils DataPack"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\saves\Datapack island\datapacks\EnergyUtils DataPack" "C:\Users\erwan\Documents\Dev\EnergyUtils\EnergyUtils DataPack"
 mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\EnergyUtils ResourcePack" "C:\Users\erwan\Documents\Dev\EnergyUtils\EnergyUtils ResourcePack"
 PAUSE
