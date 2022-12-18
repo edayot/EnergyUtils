d3111bc167edd24acc2c116dcdfe03dde9457e3b
Erwan DAYOT <pro.e.dayot@gmail.com>
2022-12-18 16:50:13 +0100

    changes to 1.19.3 textures format

--- links.sh
+++ links.sh
@@ -1,9 +1,9 @@
 
 # Copy datapack to saves/world/datapacks
-ln -s "/home/erwan/Documents/Dev/EnergyUtils/EnergyUtils DataPack" "/home/erwan/.instances/fabric1.19.2/saves/Datapack island/datapacks/EnergyUtils DataPack"
-ln -s "/home/erwan/Documents/Dev/SimplEnergy" "/home/erwan/.instances/fabric1.19.2/saves/Datapack island/datapacks/SimplEnergy"
+ln -s "/home/erwan/Documents/Dev/EnergyUtils/EnergyUtils DataPack" "/home/erwan/.config/gdlauncher_next/datapacks/EnergyUtils DataPack"
+ln -s "/home/erwan/Documents/Dev/SimplEnergy" "/home/erwan/.config/gdlauncher_next/datapacks/SimplEnergy"
 
 
 # Copy resource pack to saves/world/resourcepacks
-ln -s "/home/erwan/Documents/Dev/EnergyUtils/EnergyUtils ResourcePack" "/home/erwan/.instances/fabric1.19.2/resourcepacks/EnergyUtils ResourcePack"
-ln -s "/home/erwan/Documents/Dev/SimplEnergy/resource pack" "/home/erwan/.instances/fabric1.19.2/resourcepacks/SimplEnergy ResourcePack"
+ln -s "/home/erwan/Documents/Dev/EnergyUtils/EnergyUtils ResourcePack" "/home/erwan/.config/gdlauncher_next/resourcepacks/EnergyUtils ResourcePack"
+ln -s "/home/erwan/Documents/Dev/SimplEnergy/resource pack" "/home/erwan/.config/gdlauncher_next/resourcepacks/SimplEnergy ResourcePack"
